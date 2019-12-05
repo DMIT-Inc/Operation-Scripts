93fab8c1f09b246ada0b2c58a398b93c7eae61ba
Austin Chan <sptrsca@gmail.com>
2019-12-04 22:01:55 -0500

    add RPS RFS optimization

--- onboot.py
+++ onboot.py
@@ -4,6 +4,7 @@ import os
 def excu_optimization(nicname):
     os.system("ethtool -G " + nicname + " rx 4096 tx 4096")
     os.system("ethtool -A " + nicname + " rx off tx off")
+    os.system("ethtool -C " + nicname + " adaptive-rx off")
     os.system("setpci -v -d 8086:10fb e6.b=2e")
     os.system("setpci -v -d 8086:154d e6.b=2e")
     Out_CPUs = subprocess.Popen(["cat /sys/class/net/" + nicname + "/device/local_cpulist"],stdout=subprocess.PIPE, shell=True)
@@ -16,6 +17,17 @@ def excu_optimization(nicname):
 
     os.system("bash /etc/set_irq_affinity.sh local " + nicname)
 
+    Out_CPUsLits = subprocess.Popen(["cat /sys/class/net/" + nicname + "/device/local_cpu"],stdout=subprocess.PIPE, shell=True)
+    cpulists, cpu_error = Out_CPUsLits.communicate()
+
+    queueslist = traversalDir_FirstDir("/sys/class/net/" + nicname + "/queues/")
+    for queue in queueslist:
+        if ("rx" in queue):
+            os.system("echo \"" + cpulists + "\" > /sys/class/net/" + nicname + "/queues/" + queue + "/rps_cpus")
+            os.system("echo 2048 > /sys/class/net/" + nicname + "/queues/" + queue + "/rps_flow_cnt")
+        if ("tx" in queue):
+            os.system("echo \"" + cpulists + "\" > /sys/class/net/" + nicname + "/queues/" + queue + "/xps_cpus")
+
 def maindef():
     Output_NICList = subprocess.Popen(["find /sys/class/net ! -type d | xargs --max-args=1 realpath | awk -F\/ '/pci/{print $NF}'", ],stdout=subprocess.PIPE, shell=True)
     nic_List, nic_error = Output_NICList.communicate()
@@ -27,8 +39,19 @@ def maindef():
         eth, eth_error = Out_ETHTOOL.communicate()
         for d_nic in eth.split(os.linesep):
             if ("ixgbe" in d_nic):
-                excu_optimization(nic)
+                excu_optimization(nicName)
 
+
+def traversalDir_FirstDir(path):
+    list = []
+    if (os.path.exists(path)):
+        files = os.listdir(path)
+        for file in files:
+            m = os.path.join(path,file)
+            if (os.path.isdir(m)):
+                h = os.path.split(m)
+                list.append(h[1])
+    return list
 
 if __name__ == "__main__":
     maindef()
---
+++ onrunning.py
@@ -0,0 +1,40 @@
+import subprocess
+import os
+import re
+
+def maindef():
+    Out_CPUsTotal = subprocess.Popen(["cat /proc/cpuinfo| grep \"processor\"| wc -l"],stdout=subprocess.PIPE, shell=True)
+    totalcpu, cpu_error = Out_CPUsTotal.communicate()
+    cpus = ""
+    tcpu = int(totalcpu)
+    if (tcpu > 1):
+        for c in range(1, tcpu):
+            cpus = cpus + "1"
+    cpus = cpus + "0"
+    pattern = re.compile('.{8}')
+    f_cpus = ','.join(pattern.findall(format(int(cpus,2),'x')))
+
+    ifslist = traversalDir_FirstDir("/sys/class/net/")
+    for ifname in ifslist:
+        if ("fw" in ifname):
+            queueslist = traversalDir_FirstDir("/sys/class/net/" + ifname + "/queues/")
+            for queue in queueslist:
+                if ("rx" in queue):
+                    os.system("echo \"" + f_cpus + "\" > /sys/class/net/" + ifname + "/queues/" + queue + "/rps_cpus")
+                    os.system("echo 2048 > /sys/class/net/" + ifname + "/queues/" + queue + "/rps_flow_cnt")
+                if ("tx" in queue):
+                    os.system("echo \"" + f_cpus + "\" > /sys/class/net/" + ifname + "/queues/" + queue + "/xps_cpus")
+
+def traversalDir_FirstDir(path):
+    list = []
+    if (os.path.exists(path)):
+        files = os.listdir(path)
+        for file in files:
+            m = os.path.join(path,file)
+            if (os.path.isdir(m)):
+                h = os.path.split(m)
+                list.append(h[1])
+    return list
+
+if __name__ == "__main__":
+    maindef()
